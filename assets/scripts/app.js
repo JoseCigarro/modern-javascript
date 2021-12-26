aeea6d3186b41cbe1d6cf0bd1752aedf68e7c55c
José Cigarro <jmcigarro87@gmail.com>
2021-12-26 19:25:55 +0000

    Writing a function with return

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,13 +4,10 @@ let currentResult = defaultResult;
 
 function add(num1, num2) {
     const result = num1 + num2;
-    alert(result);
+    return result;
 }
 
-add(1, 3);
-add(5, 5);
-
-currentResult = (currentResult + 10) * 9 / 4 - 1;
+currentResult = add(1, 3); 
 
 // let calculationDescription = '(' + defaultResult + ' + 10) * 9 / 4 - 1';
 // You can use the backticks and the dolar sign and the curly brackets to inject the value of a variable in a String 👍
